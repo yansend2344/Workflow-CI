258e71094829d2ba7e95cbcce0c459341d222e1e
yansend2344 <yansen.demos@gmail.com>
2025-12-22 16:29:37 +0700

    perubahan pada modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,24 +6,19 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import os
 
-INPUT_PATH = "heartdisease_preprocessing.csv"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+INPUT_PATH = os.path.join(BASE_DIR, "heartdisease_preprocessing.csv")
 
 def main():
-    print("--- Memulai Workflow MLProject ---")
-
-    # Load Data
     if not os.path.exists(INPUT_PATH):
-        print(f"ERROR: {INPUT_PATH} tidak ditemukan!")
-        return
+        raise FileNotFoundError(f"File {INPUT_PATH} tidak ditemukan.")
         
     df = pd.read_csv(INPUT_PATH)
     X = df.drop(columns=['num'])
     y = df['num']
     
-    # Split Data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Training
     mlflow.sklearn.autolog()
     
     with mlflow.start_run(run_name="Docker_CI_Run"):
@@ -36,11 +31,9 @@ def main():
         
         model.fit(X_train, y_train)
         
-        # Evaluasi
         y_pred = model.predict(X_test)
         acc = accuracy_score(y_test, y_pred)
         
-        print(f"Training Selesai.")
         print(f"Akurasi Model: {acc:.4f}") 
 
 if __name__ == "__main__":
